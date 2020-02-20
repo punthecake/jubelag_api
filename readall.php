81f6748fccc144219ad347b5b53990c629635ba0
punthecake <60122576+punthecake@users.noreply.github.com>
2020-02-20 11:27:53 +0800

    Create readall.php

---
+++ readall.php
@@ -0,0 +1,27 @@
+<?php
+$json = file_get_contents("http://rdapi.herokuapp.com/product/read.php");
+
+$data = json_decode($json,true);
+$list = $data['records'];
+
+
+?>
+
+<h1>Products </h1>
+
+<table border="1px">
+    <tr>
+        <td>Name</td>
+        <td>Price</td>
+    </tr>
+<?php
+foreach($list as $value){
+    ?>
+    <tr>
+        <td><?php echo $value['name'];?></td>
+        <td><?php echo $value['price'];?></td>
+    </tr>
+<?php
+}
+?>
+</table>
