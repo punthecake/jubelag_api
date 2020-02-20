fa1c91a8d60bfd94d9f3fb359a9cfb21b08fd24e
punthecake <60122576+punthecake@users.noreply.github.com>
2020-02-20 11:36:08 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,32 @@
+<?php 
+$loadnav = (isset($_GET['loadnav']) && $_GET['loadnav'] != '') ? $_GET['loadnav']: '';
+?>
+<html> 
+    <head>
+        <title>  API Test </title>
+    </head>
+    <body>
+    <div>               
+                        <div>
+                        <ul> 
+                        <li> <a href="index.php?loadnav=home"> HOME </a> </li>
+                        <li> <a href="index.php?loadnav=create">Create </a> </li>
+                        </ul>
+                        </div>
 
+                        <div>
+                        <?php 
+                        switch($loadnav){
+                            case 'create':
+                            require_once 'form_create.php';
+                            break;
+                            default:
+                            require_once 'home.php';
+                            break;
+                        }
+                        ?>
+                        </div>
+                        </div>
+    </div>
+    </body>
+</html>
