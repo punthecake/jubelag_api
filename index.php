e17c5782089ea4fa4f20d388a6a613c3ee33cccf
punthecake <60122576+punthecake@users.noreply.github.com>
2020-02-20 11:52:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,6 @@ $loadnav = (isset($_GET['loadnav']) && $_GET['loadnav'] != '') ? $_GET['loadnav'
                         <li> <a href="index.php?loadnav=home"> Home </a> </li>
                         <li> <a href="index.php?loadnav=create">Create </a> </li>
                         <li> <a href="index.php?loadnav=read">Display Products </a> </li>
-                        <li> <a href="index.php?loadnav=category">Display Categories </a> </li>
                         </ul>
                         </div>
 
@@ -24,9 +23,6 @@ $loadnav = (isset($_GET['loadnav']) && $_GET['loadnav'] != '') ? $_GET['loadnav'
                             break;
                             case 'read':
                             require_once 'readall.php';
-                            break;
-                            case:'category':
-                            require_once 'category.php';
                             break;
                             default:
                             require_once 'home.php';
